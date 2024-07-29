e9f923a21da0e91f09e0569ca9fbe669b56cbad2
Drcodepro <drrahangdale01@gmail.com>
2024-07-29 11:37:29 +0530

    neww change

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 
-const menue_btn = document.querySelector(".toggle_nav_button");
+const menue_btn = document.getElementById("toggle_nav_button");
 const nav_container = document.querySelector(".nav_navigation_container");
 
 const HandleNav_visible = ()=>{
-    console.log("dkjfno")
     nav_container.classList.toggle("navigation_mobile");
 }
 menue_btn.addEventListener("click",HandleNav_visible)
