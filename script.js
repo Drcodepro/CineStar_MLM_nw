8ce7a0541c4037bcd877e426e6270cfdb2e2b39a
Drcodepro <drrahangdale01@gmail.com>
2024-07-29 11:08:58 +0530

    new

--- script.js
+++ script.js
@@ -1,21 +1,9 @@
 
-// let show = false;
-// const menue_btn = document.querySelector(".toggle_nav_button");
-// const nav_container = document.querySelector(".nav_navigation_container");
+const menue_btn = document.querySelector(".toggle_nav_button");
+const nav_container = document.querySelector(".nav_navigation_container");
 
-// HandleNav_visible=()=>{
-//     console.log("bahar")
-//     if (show) {
-//         console.log("clicked_shw");
-
-//         nav_container.style.display = "none"; 
-//         show = !show;
-//     }
-//     else{
-//         console.log("clicked_hiden")
-
-//         nav_container.style.display = "block"; 
-//         show = !show;
-//     }
-// }
-// menue_btn.addEventListener("click",HandleNav_visible)
+const HandleNav_visible = ()=>{
+    console.log("dkjfno")
+    nav_container.classList.toggle("navigation_mobile");
+}
+menue_btn.addEventListener("click",HandleNav_visible)
