880f41fe67172072d04af3db36fd7136074c70e4
Drcodepro <drrahangdale01@gmail.com>
2024-07-29 12:46:23 +0530

    neww

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 
 const menue_btn = document.getElementById("toggle_nav_button");
 const nav_container = document.querySelector(".nav_navigation_container");
+const navigation = document.querySelector("#navigation_text");
 
 const HandleNav_visible = ()=>{
+    console.log("clicked");
     nav_container.classList.toggle("navigation_mobile");
 }
-menue_btn.addEventListener("click",HandleNav_visible)
-
+menue_btn.addEventListener("click",HandleNav_visible);
+navigation.addEventListener("click",HandleNav_visible);
 
 
 // ppt open on click btn
